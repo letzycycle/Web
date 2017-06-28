c9e7c07d9ba93d2dd966cb9970cd9fbf3e48ae39
Mohit <mohitkaushal91@gmail.com>
2017-06-28 08:54:19 +0530

    intial uploading of contents

---
+++ js/custom.js
@@ -0,0 +1,146 @@
+(function() {
+    "use strict";
+    $('#loader').delay(700).fadeOut();
+    $('#mask').delay(1200).fadeOut("slow");	
+	
+	// Back to Top On Scroll
+	$(window).on('scroll', function() {        
+        if ($(this).scrollTop() > 100) {
+			$('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+	
+	$('#back-to-top').on('click', function(){
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, 1000
+		);
+	});// END Back to Top On Scroll
+	
+	
+    
+    //WOW JS
+    new WOW().init();
+	
+
+
+    // Full page BG
+    $('.bg-full').each(function() {
+        var $src = $(this).attr('src');
+        $(this).parent().css({
+            'background': 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), rgba(0, 0, 0, 0.4) url(' + $src + ')',
+            'background-position': 'center top',
+            'background-repeat': 'no-repeat',
+            'background-attachment': 'fixed',
+            'background-size': 'cover',
+        });
+        $(this).remove();
+    });
+
+    
+
+
+
+    $('#video-item').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+	
+	$('.screenshot-items').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true,
+        focusOnSelect: true,
+        dots: true,
+        centerPadding: '228px',
+        arrows: false,
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                centerPadding: '188px',
+            }
+        }, {
+            breakpoint: 970,
+            settings: {
+                arrows: false,
+                centerPadding: '144px',
+            }
+        }, {
+            breakpoint: 767,
+            settings: {
+                arrows: false,
+                centerPadding: '0px',
+            }
+        }, {
+            breakpoint: 350,
+            settings: {
+                arrows: false,
+                centerPadding: '0px',
+                dots: true,
+                slidesToShow: 1,
+            }
+        }, ]
+    });
+
+    $.extend(true, $.magnificPopup.defaults, {
+        iframe: {
+            patterns: {
+                youtube: {
+                    index: 'youtube.com/',
+                    id: 'v=',
+                    src: 'http://www.youtube.com/embed/%id%?autoplay=1'
+                }
+            }
+        }
+    });
+
+
+    if (document.getElementsByClassName("team").length) {
+        var eventFired = false,
+            objectPositionTop = $('.team').offset().top;
+    }
+    $(window).on('scroll', function() {
+        // Counter On Scroll
+        var currentPosition = $(document).scrollTop();
+        if (currentPosition > objectPositionTop && eventFired === false) {
+            eventFired = true;
+            $('.number').each(function() {
+                $(this).prop('count', 100).animate({
+                    count: $(this).text()
+                }, {
+                    duration: 4000,
+                    easing: 'swing',
+                    step: function(now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+        }
+
+    });
+
+    $('.carousel-control').on('click', function() {
+        $('#tmCarousel').carousel($(this).data());
+    });
+
+
+    // Contact Form
+    $('.contact-form').on('submit', function(e) {
+        e.preventDefault();
+        $.post('mail/send.php', $(this).serialize()).done(function(data) {
+            $('.contact-form').fadeOut('slow', function() {
+                $('.contact-form').fadeIn('slow').html(data);
+            });
+        }).fail(function() {
+            alert('SOMETHING WENT WRONG! PLEASE TRY AGAIN.');
+        });
+    });
+
+})();
---
+++ js/ytd-video.js
@@ -0,0 +1,21 @@
+/*function onYouTubeIframeAPIReady() {
+var player;
+player = new YT.Player('zizo-videobg', {
+videoId: 'ndeCb6_4U4U', // YouTube Video ID
+playerVars: {
+autoplay: 1, // Auto-play the video on load
+rel: 0, // Related video show off
+controls: 0, // Show pause/play buttons in player
+showinfo: 0, // Hide the video title
+modestbranding: 0, // Hide the Youtube Logo
+loop: 1, // Run the video in a loop
+playlist: 'ndeCb6_4U4U', // If loop: 1 please add same Video id for playlist.
+fs: 0, // Hide the full screen button
+},
+events: {
+onReady: function(e) {
+e.target.mute();
+}
+}
+});
+} */
